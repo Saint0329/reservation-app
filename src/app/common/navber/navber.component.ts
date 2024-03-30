343a41c063b879e525a3dc382ce5075a3d4c47d2
Seiya <x_10afreedomgundam@yahoo.co.jp>
2024-03-30 15:50:59 +0900

    ヘッダーコンポーネント追加

---
+++ src/app/common/navber/navber.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-navber',
+  standalone: true,
+  templateUrl: './navber.component.html',
+  styleUrl: './navber.component.scss'
+
+})
+export class NavberComponent {
+
+}
