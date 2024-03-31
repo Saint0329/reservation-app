eee1ebdf3f8b68d91c169161ef34d4da1aa9b89d
Seiya <x_10afreedomgundam@yahoo.co.jp>
2024-03-31 22:38:35 +0900

    サーバー

---
+++ sever/routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router()
+const Product = require('../model/product');
+
+router.get('', async function(req, res) {
+    foundProducts = await Product.find({})
+    res.json(foundProducts)
+})
+
+//router.get('/:productId', async function(req, res) {
+//    const productId = req.params.productId
+//    foundProduct = await Product.findById({})
+//    res.json(foundProduct)
+//})
+
+router.get('/:productId', async function(req, res) {
+    const productId =req.params.productId
+    try {
+        foundProduct = await Product.findById(productId)
+        return res.json(foundProduct)
+    } catch(e) {
+        return res.status(422).send({errors: [{title: 'Product error', detail: 'Product not found!'}]})
+    }
+})
+
+module.exports = router
